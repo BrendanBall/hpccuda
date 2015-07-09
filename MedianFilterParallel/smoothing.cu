0e1b942f4060a5869b7de2e92f8e8f6a709e34a9
BrendanBall <ball.brendan50@gmail.com>
2015-07-09 20:26:05 +0200

    added 3x3 filter cuda kernel

--- MedianFilterParallel/smoothing.cu
+++ MedianFilterParallel/smoothing.cu
@@ -4,6 +4,7 @@
 #include <math.h> 
 #include <cstring>
 #include "cuda_runtime.h"
+#include "device_functions.h"
 #include "device_launch_parameters.h"
 #include <thrust/sort.h>
 
@@ -17,6 +18,129 @@ hpcparallel::smoothing::smoothing(int resolution, int binsize, int* bins, int fi
 	//memset(filteredBins, 0, binsize * sizeof(int));
 
 
+}
+
+__global__ void medianFilter3x3Kernel(const int* dev_bins, int* dev_filteredBins, int resolution, int binsize, int filtersize, int halfFS, int windowsize)
+{
+	int tx = blockDim.x * blockIdx.x + threadIdx.x;
+	int ty = blockDim.y * blockIdx.y + threadIdx.y;
+	int tlx = threadIdx.x;
+	int tly = threadIdx.y;
+
+	if (tx < resolution && ty < resolution)
+	{
+		//shared array is 1 row/column bigger on every side for edge cases.
+		__shared__ int sm_bins[BLOCKSIZE + 2][BLOCKSIZE + 2];
+
+		//populate shared memory block
+		sm_bins[threadIdx.y + 1][threadIdx.x + 1] = dev_bins[ty*resolution + tx];
+
+		//get edge values needed for sliding window
+		bool tx_left_edge = (tlx == 0);
+		bool ty_top_edge = (tly == 0);
+		bool tx_right_edge = (tlx == BLOCKSIZE - 1);
+		bool ty_bot_edge = (tly == BLOCKSIZE - 1);
+
+		// -1 padding for values not in dev_bins
+		if (tx_left_edge)
+			sm_bins[tly + 1][tlx] = -1;
+		else if (tx_right_edge)
+			sm_bins[tly + 1][tlx + 2] = -1;
+		if (ty_top_edge)
+		{
+			sm_bins[tly][tlx + 1] = -1;
+			if (tx_left_edge)
+				sm_bins[tly][tlx] = -1;
+			else if (tx_right_edge)
+				sm_bins[tly][tlx + 2] = -1;
+
+		}
+		else if (ty_bot_edge)
+		{
+			sm_bins[tly + 2][tlx + 1] = -1;
+			if (tx_left_edge)
+				sm_bins[tly + 2][tlx] = -1;
+			else if (tx_right_edge)
+				sm_bins[tly + 2][tlx + 2] = -1;
+		}
+
+
+		//check if shared memory edge is global edge and then don't include it
+		tx_left_edge &= (tx > 0);
+		tx_right_edge &= (tx < resolution - 1);
+		ty_top_edge &= (ty > 0);
+		ty_bot_edge &= (ty < resolution - 1);
+
+		// pull values into shared memory
+		if (tx_left_edge)
+			sm_bins[tly + 1][tlx] = dev_bins[ty*resolution + tx - 1];
+		else if (tx_right_edge)
+			sm_bins[tly + 1][tlx + 2] = dev_bins[ty*resolution + tx + 1];
+		if (ty_top_edge)
+		{
+			sm_bins[tly][tlx + 1] = dev_bins[(ty - 1)*resolution + tx];
+			if (tx_left_edge)
+				sm_bins[tly][tlx] = dev_bins[(ty - 1)*resolution + tx - 1];
+			else if (tx_right_edge)
+				sm_bins[tly][tlx + 2] = dev_bins[(ty - 1)*resolution + tx + 1];
+
+		}
+		else if (ty_bot_edge)
+		{
+			sm_bins[tly + 2][tlx + 1] = dev_bins[(ty + 1)*resolution + tx];
+			if (tx_left_edge)
+				sm_bins[tly + 2][tlx] = dev_bins[(ty + 1)*resolution + tx - 1];
+			else if (tx_right_edge)
+				sm_bins[tly + 2][tlx + 2] = dev_bins[(ty + 1)*resolution + tx + 1];
+		}
+
+		__syncthreads();
+
+		int window[9] =
+		{
+			sm_bins[threadIdx.y][threadIdx.x], sm_bins[threadIdx.y][threadIdx.x + 1], sm_bins[threadIdx.y][threadIdx.x + 2],
+			sm_bins[threadIdx.y + 1][threadIdx.x], sm_bins[threadIdx.y + 1][threadIdx.x + 1], sm_bins[threadIdx.y + 1][threadIdx.x + 2],
+			sm_bins[threadIdx.y + 2][threadIdx.x], sm_bins[threadIdx.y + 2][threadIdx.x + 1], sm_bins[threadIdx.y + 2][threadIdx.x + 2]
+		};
+
+	
+		//thrust::sort(thrust::seq, window, window + 9);
+
+		//bubble sort
+		for (int i = 0; i < 9; ++i){
+		
+			for (int j = i + 1; j < 9; ++j){
+				if (window[i] > window[j])
+				{
+					int temp = window[i];
+					window[i] = window[j];
+					window[j] = temp;
+				}
+			}
+		}
+		
+		int edges = 0; // count number of elements equal to -1 which are not in the global array
+		for (int i = 0; i < 9; ++i)
+		{
+			if (window[i] == -1)
+				edges++;
+		}
+
+		int median;
+		int i = (9 - edges);
+		if (i % 2 == 0)
+		{
+			//throwing away decimal part of average
+			median = ((window[(i / 2) - 1 + edges] + window[(i / 2) + edges]) / 2);
+		}
+		else
+		{
+			median = window[(i / 2) + edges];
+		}
+		
+			
+		dev_filteredBins[ty * resolution + tx] = median;
+	}
 }
 
 __global__ void medianFilterKernel(const int* dev_bins, int* dev_filteredBins, int resolution, int binsize, int filtersize, int halfFS, int windowsize)
@@ -79,11 +203,9 @@ inline void gpuAssert(cudaError_t code, char* description, char *file, int line,
 
 int* hpcparallel::smoothing::applyFilter()
 {
-	//for (int i = 0; i < binsize; ++i)
-	//{
-	//	//std::cout << i << std::endl;
-	//	filteredBins[i] = median(i);
-	//}
+	struct cudaFuncAttributes funcAttrib;
+	cudaSafe(cudaFuncGetAttributes(&funcAttrib, medianFilter3x3Kernel), "cudafuncgetattributes");
+	printf("%s numRegs=%d\n", "medianFilter3x3Kernel", funcAttrib.numRegs);
 	int* dev_bins = 0;
 	int* dev_filteredBins = 0;
 	cudaMedianFilter(dev_bins, dev_filteredBins);
@@ -112,7 +234,8 @@ void hpcparallel::smoothing::cudaMedianFilter(int* dev_bins, int* dev_filteredBi
 	//std::cout << "numBlocks & numThreads: " << numBlocks.x << " " << numThreads.x << " " << numBlocks.x * numThreads.x << " " << resolution << std::endl;
 	//std::cout << "numBlocks & numThreads: " << numBlocks.y << " " << numThreads.y << " " << numBlocks.y * numThreads.y << " " << resolution << std::endl;
 
-	medianFilterKernel << <numBlocks, numThreads >> >(dev_bins, dev_filteredBins, resolution, binsize, filtersize, int(filtersize / 2), filtersize*filtersize);
+	//medianFilterKernel << <numBlocks, numThreads >> >(dev_bins, dev_filteredBins, resolution, binsize, filtersize, int(filtersize / 2), filtersize*filtersize);
+	medianFilter3x3Kernel << <numBlocks, numThreads >> >(dev_bins, dev_filteredBins, resolution, binsize, 3, 1, 9);
 
 	cudaSafe(cudaGetLastError(), "cuda launch");
 	
@@ -123,67 +246,6 @@ void hpcparallel::smoothing::cudaMedianFilter(int* dev_bins, int* dev_filteredBi
 	cudaSafe(cudaFree(dev_bins), "cuda free");
 	cudaSafe(cudaFree(dev_filteredBins), "cuda free");
 }
-
-
-
-int hpcparallel::smoothing::median(int currentBin)
-{
-
-	int binx = currentBin % resolution;
-	int biny = currentBin / resolution;
-	int halfFS = int(filtersize / 2);
-	int startx = binx - halfFS;
-	int starty = biny - halfFS;
-	int endx = startx + filtersize;
-	int endy = starty + filtersize;
-
-	//std::cout << "binxy"<< binx << " " << biny << std::endl;
-
-	//std::cout << startx << " " << endx << " " << starty << " " <<endy << std::endl;
-
-
-
-
-	int i = 0;
-	for (int y = starty; y < endy; ++y)
-	{
-		for (int x = startx; x < endx; ++x)
-		{
-			if (y >= 0 && y < resolution && x >= 0 && x < resolution)
-			{
-				window[i] = bins[y*resolution + x];
-				++i;
-			}
-		}
-
-	}
-
-
-	std::sort(window, window + i);
-
-	/*for (int j = 0; j < i; ++j)
-	{
-	if (j % filtersize == 0)
-	{
-	std::cout << std::endl;
-	}
-	std::cout << window[j] << " ";
-	}*/
-
-
-	int median;
-	if (i % 2 == 0)
-	{
-		//throwing away decimal part of average
-		median = (window[(i / 2) - 1] + window[(i / 2)]) / 2;
-	}
-	else
-	{
-		median = window[(i / 2)];
-	}
-	return median;
-}
-
 
 
 hpcparallel::smoothing::~smoothing()
